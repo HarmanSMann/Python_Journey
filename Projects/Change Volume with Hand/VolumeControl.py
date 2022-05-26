94e3a8c453662acd8e416eccbc0da0b20b8dc421
HarmanSMann <76751076+HarmanSMann@users.noreply.github.com>
2022-05-26 14:35:26 -0400

    Update VolumeControl.py
    Added some minor changes towards the project:
    1 - a volume Bar to indicate the range of your volume
    2 - a percentage under the bar for the scale
    
    Current issues:
    
    because of how volume has calculated the range for each of these is different

--- Projects/Change Volume with Hand/VolumeControl.py
+++ Projects/Change Volume with Hand/VolumeControl.py
@@ -20,9 +20,18 @@ volume.SetMasterVolumeLevel(-20.0, None)
 volRange = volume.GetVolumeRange()
 minVol = volRange[0]
 maxVol = volRange[1]
+volBar = 400
+volPer = 0
 
 ##########################################
 wCam, hCam = 640, 480
+
+# Current issue: scaling the volume to match with the percentage bar
+# vol: -65 to 0
+# bar: 150 to 400
+# Per: 0 to 100
+# at 50% Percent -> bar 50%, -> vol at -27 -> scaling is off
+# But I believe that's because how volume is calculated
 ##########################################
 
 cap = cv2.VideoCapture(1)
@@ -37,7 +46,7 @@ while True:
     img = detector.findHands(img, draw=False)
     lmList = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
-        print(lmList[4], lmList[8])
+        # print(lmList[4], lmList[8])
         x1, y1 = lmList[4][1], lmList[4][2]
         x2, y2 = lmList[8][1], lmList[8][2]
         cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
@@ -45,23 +54,31 @@ while True:
         cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
         cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
         cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-        cv2.circle(img, (cx, cy), 15, (255, 255, 255), cv2.FILLED)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
         length = math.hypot(x2 - x1, y2 - y1)
-        print(length)
 
-        vol = np.interp(length, [50, 300], [minVol, maxVol])
-        print(vol)
+        # value to check, range to check, range to self
+        vol = np.interp(length, [50, 400], [minVol, maxVol])
+        volBar = np.interp(vol, [minVol, maxVol], [400, 150])
+        volPer = np.interp(vol, [minVol, maxVol], [0, 100])
+
         volume.SetMasterVolumeLevel(vol, None)
 
         if length < 50:
-            cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+            cv2.circle(img, (cx, cy), 15, (0, 0, 255), cv2.FILLED)
+
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450),
+                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv2.putText(img, str(int(fps)), (10, 70),
+                cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
