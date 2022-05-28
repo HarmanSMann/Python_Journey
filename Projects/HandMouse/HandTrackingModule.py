c59d1cc8090125c4a98629fd458fa0eb415170aa
HarmanSMann <76751076+HarmanSMann@users.noreply.github.com>
2022-05-27 20:32:39 -0400

    Create HandTrackingModule.py

---
+++ Projects/HandMouse/HandTrackingModule.py
@@ -0,0 +1,105 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class handDetector():
+    def __int__(self, mode=False, maxHands=2, detectionCon=0.5, trackingCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackingCon = trackingCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(
+            self.mode, self.maxHands, self.detectionCon, self.trackingCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipTds = [4, 8, 12, 16, 20]
+        self.lmList = []
+
+    def findHands(self, img, draw=True):  # draws the dots and lines
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        # detect the hand
+        # print(result.multi_hand_landmarks)
+
+        if self.results.multi_hand_landmarks:
+            for handLMs in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(
+                        img, handLMs, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, hand_num=0, draw=True):  # draws the position too
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[hand_num]
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                # print(id, cx, cy)
+                lmList = ([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+        return lmList
+
+    def fingersUp(self):
+        fingers = []
+        # thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        print(fingers)
+
+        totalFingers = fingers.count(1)
+        print(totalFingers)
+
+        return fingers
+
+    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(1)
+    detector = handDetector
+    while True:
+        success, img = cap.read()
+        img = detector.findHands()
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[8])  # throw the id of whatever you want to print
+
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
