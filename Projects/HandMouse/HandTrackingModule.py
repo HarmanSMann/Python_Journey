13858c4397a63977af96389cb07e2b6e6356144f
HarmanSMann <76751076+HarmanSMann@users.noreply.github.com>
2022-05-30 15:12:59 -0400

    Update HandTrackingModule.py
    Cleaning up some of the process
    It was full of bugs/changes that needed to be implemented because of updates to mediapipe and other things

--- Projects/HandMouse/HandTrackingModule.py
+++ Projects/HandMouse/HandTrackingModule.py
@@ -1,10 +1,12 @@
+import math
+
 import cv2
 import mediapipe as mp
 import time
 
 
 class handDetector():
-    def __int__(self, mode=False, maxHands=2, detectionCon=0.5, trackingCon=0.5):
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackingCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
@@ -14,8 +16,9 @@ class handDetector():
         self.hands = self.mpHands.Hands(
             self.mode, self.maxHands, self.detectionCon, self.trackingCon)
         self.mpDraw = mp.solutions.drawing_utils
-        self.tipTds = [4, 8, 12, 16, 20]
+        self.tipIds = [4, 8, 12, 16, 20]
         self.lmList = []
+        self.results = None
 
     def findHands(self, img, draw=True):  # draws the dots and lines
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -26,22 +29,35 @@ class handDetector():
         if self.results.multi_hand_landmarks:
             for handLMs in self.results.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(
-                        img, handLMs, self.mpHands.HAND_CONNECTIONS)
+                    self.mpDraw.draw_landmarks(img, handLMs, self.mpHands.HAND_CONNECTIONS)
         return img
 
     def findPosition(self, img, hand_num=0, draw=True):  # draws the position too
+        xList = []
+        yList = []
+        bbox = []
+        self.lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[hand_num]
             for id, lm in enumerate(myHand.landmark):
                 # print(id, lm)
                 h, w, c = img.shape
-                cx, cy = int(lm.x*w), int(lm.y*h)
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
                 # print(id, cx, cy)
-                lmList = ([id, cx, cy])
+                self.lmList.append([id, cx, cy])
+                # Draw each point here
                 if draw:
-                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-        return lmList
+                    cv2.circle(img, (cx, cy), 7, (0, 0, 255), cv2.FILLED)
+
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
+
+            if draw:
+                cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20), (0, 255, 0), 2)
+        return self.lmList, bbox
 
     def fingersUp(self):
         fingers = []
@@ -81,17 +97,15 @@ class handDetector():
 def main():
     pTime = 0
     cTime = 0
-    cap = cv2.VideoCapture(1)
-    detector = handDetector
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
     while True:
         success, img = cap.read()
-        img = detector.findHands()
-        lmList = detector.findPosition(img)
-        if len(lmList) != 0:
-            print(lmList[8])  # throw the id of whatever you want to print
+        img = detector.findHands(img)
+        lmList, bbox = detector.findPosition(img)  # This prints only the last element in the list weird
 
         cTime = time.time()
-        fps = 1/(cTime-pTime)
+        fps = 1 / (cTime - pTime)
         pTime = cTime
 
         cv2.putText(img, str(int(fps)), (10, 70),
